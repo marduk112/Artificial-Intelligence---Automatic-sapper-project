d5593e69f66cca74b5a40ebb33c2898d01d6285c
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-04-26 19:31:41 +0200

    Added movement for sapper and change architecture
    Move code from code behind to ViewModel for compliance with MVVM

--- AutomaticSapper/MainWindow.xaml.cs
+++ AutomaticSapper/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Timers;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using KnowledgeRepresentation.Bombs;
+using System.Windows.Threading;
+using AutomaticSapper.ViewModel;
 
 
 namespace AutomaticSapper
@@ -23,80 +12,23 @@ namespace AutomaticSapper
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static Button playerX;
         public MainWindow()
         {
-            List<List<String>> lsts = new List<List<String>>();
-
-            BallBomb d = new BallBomb();
-            Random rnd = new Random();
-            String text;
-            playerX = bu;
-            
-            Timer aTimer = new Timer(100);
-            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            aTimer.Enabled = true;
-
-            for (int i = 0; i < 20; i++)
-            {
-                lsts.Add(new List<String>());
-
-                for (int j = 0; j < 20; j++)
-                {
-                    int rand = rnd.Next(25);
-                    switch (rand)
-                    {
-                        case 0:
-                            text = "0";
-                            break;
-                        case 1:
-                            text = "1";
-                            break;
-                        case 2:
-                            text = "2";
-                            break;
-                        case 3:
-                            text = "3";
-                            break;
-                        case 4:
-                            text = "4";
-                            break;
-                        default:
-                            text = "";
-                            break;
-                    }
-
-                    lsts[i].Add(text);  
-                }
-
-                
-            }
-
             InitializeComponent();
-
-            lst.ItemsSource = lsts;
-            
+            DataContext = new MainWindowViewModel();
         }
-
-        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            seT();
-
+            // The ViewModel object that needs to marshal some actions is
+            // attached as the DataContext by the time of the loaded event.
+            var vmTest = (DataContext as MainWindowViewModel);
+            if (null != vmTest)
+            {
+                // Set the ViewModel's reference SynchronizationContext to
+                // the View's current context.
+                vmTest.ViewContext = Dispatcher.Invoke
+                    (() => SynchronizationContext.Current);
+            }
         }
-
-        private static void seT()
-        {
-            //MainWindow myWindow = Application.Current.MainWindow as MainWindow;
-            //Button myButton = myWindow.bu;
-
-            //double actualTop = Canvas.GetTop(myButton);
-            //Canvas.SetTop(bu, 100);
-        }
-
-        private void bu_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-    
     }
 }
---
+++ AutomaticSapper/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Timers;
+using System.Windows.Media;
+using AutomaticSapper.Annotations;
+using Timer = System.Timers.Timer;
+
+namespace AutomaticSapper.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<List<String>> Bombs { get; private set; }
+        public SynchronizationContext ViewContext { get; set; }
+
+        public int Width
+        {
+            get { return _width; }
+            set
+            {
+                _width = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set
+            {
+                _height = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainWindowViewModel()
+        {
+            Bombs = new ObservableCollection<List<String>>();
+            SapperTransform = new TranslateTransform();
+            Width = 525;
+            Height = 350;
+            var aTimer = new Timer(10);
+            aTimer.Elapsed += aTimer_Elapsed;
+            aTimer.Enabled = true;
+            CreateBoard();
+        }
+
+        void aTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (ViewContext != null)
+            {
+                ViewContext.Post(_ =>
+                {
+                    if (_x >= Width)
+                    {
+                        _y += 26;
+                        _isIncrement = false;
+                    }
+                    if (_x <= -10)
+                    {
+                        _y += 26;
+                        _isIncrement = true;
+                    }
+                    SapperTransform = new TranslateTransform(_x, _y);
+                    if (_isIncrement)
+                        _x++;
+                    else
+                        _x--;
+                }, null);
+            }
+        }
+
+        public TranslateTransform SapperTransform
+        {
+            get { return _sapperTransform; }
+            set
+            {
+                _sapperTransform = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void CreateBoard()
+        {
+            var rnd = new Random();
+            string text;
+            var tempList = new List<String>();
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    Bombs.Add(new List<string>());
+                    int rand = rnd.Next(25);
+                    switch (rand)
+                    {
+                        case 0:
+                            text = "0";
+                            break;
+                        case 1:
+                            text = "1";
+                            break;
+                        case 2:
+                            text = "2";
+                            break;
+                        case 3:
+                            text = "3";
+                            break;
+                        case 4:
+                            text = "4";
+                            break;
+                        default:
+                            text = "";
+                            break;
+                    }
+                    Bombs[i].Add(text);
+                }
+            }
+        }
+
+        private static int _x = 10, _y = 8;
+        private TranslateTransform _sapperTransform;
+        private int _width, _height;
+        private bool _isIncrement = true;
+    }
+}
